8ef95011728b44a21bd6aa55877b95ce43470b36
Henrik Rydgård <hrydgard@gmail.com>
2020-07-04 23:14:53 +0200

    Forgot to add this

--- Core/Core.cpp
+++ Core/Core.cpp
@@ -409,15 +409,14 @@ void Core_MemoryException(u32 address, u32 pc, MemoryExceptionType type) {
 		WARN_LOG(MEMMAP, "%s: Invalid address %08x PC %08x LR %08x", desc, address, currentMIPS->pc, currentMIPS->r[MIPS_REG_RA]);
 	}
 
-	ExceptionInfo &e = g_exceptionInfo;
-	e = {};
-	e.type = ExceptionType::MEMORY;
-	e.info = "";
-	e.memory_type = type;
-	e.address = address;
-	e.pc = pc;
-
 	if (!g_Config.bIgnoreBadMemAccess) {
+		ExceptionInfo &e = g_exceptionInfo;
+		e = {};
+		e.type = ExceptionType::MEMORY;
+		e.info = "";
+		e.memory_type = type;
+		e.address = address;
+		e.pc = pc;
 		Core_EnableStepping(true);
 		host->SetDebugMode(true);
 	}
